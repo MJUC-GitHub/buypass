cd7f2b99877e304a58b7e70746d520b231991005
otoneko <tomo962115@gmail.com>
2023-12-02 18:27:38 +0900

    Update

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function buypass() {
-  if (!window.PaymentRequest) return alert("Payment Request APIに未対応なため使えません");
+  if (!window.PaymentRequest)
+    return alert("Payment Request APIに未対応なため使えません");
 
-  new PaymentRequest(
+  const paymentRequest = new PaymentRequest(
     [
       {
         supportedMethods: location.origin + "/payment-manifest.json",
         data: {
-          url: document.querySelector("input").value
+          url: document.querySelector("input").value,
         },
       },
     ],
@@ -14,11 +15,18 @@ function buypass() {
       total: {
         label: "_",
         amount: {
-          value: "1", currency: "USD"
+          value: "1",
+          currency: "USD",
         },
       },
     }
   ).show();
+  paymentRequest.onshow = (event) => {
+    const modal = event.openWindow();
+    modal.style.width = "500px"; // 適切なサイズに変更
+    modal.style.height = "500px";
+  };
+  paymentRequest.show();
 }
 
 document.querySelector("button").onclick = buypass;
